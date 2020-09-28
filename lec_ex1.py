297a1c6d8fc6467a3154b52b03a770079fc9431f
strongholdleg <71986508+strongholdleg@users.noreply.github.com>
2020-09-28 17:38:45 +0200

    Add files via upload
    прослушали лекцию 2 попробовали делать задания

---
+++ lec_ex1.py
@@ -0,0 +1,10 @@
+a = 3
+if a > 5 :
+    print('hello')
+elif a < 2:
+    print('hello')
+else:
+    print('Tupo hello')
+
+
+
---
+++ lec_ex10.py
@@ -0,0 +1,25 @@
+for i in 'hello world':
+    if i == 'o':
+            continue
+print(i * 2, end='')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
