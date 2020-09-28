781fde332cd04a7439c5811b7048f2335dbfbe33
strongholdleg <71986508+strongholdleg@users.noreply.github.com>
2020-09-28 18:41:31 +0200

    Add files via upload
    bababoy

---
+++ lab_ex2.py
@@ -0,0 +1,62 @@
+a = input()
+a = int(a)
+b = input()
+b = int(b)
+c = input()
+c = int(c)
+for i in range(a, b, с):
+    print(i, end=' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
