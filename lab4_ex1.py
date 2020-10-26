8e75f0c142253af4f92e5fa149f0ee0ef111da39
strongholdleg <71986508+strongholdleg@users.noreply.github.com>
2020-10-26 17:46:04 +0200

    1 задание с функциями
    бабабой

---
+++ lab4_ex1.py
@@ -0,0 +1,49 @@
+import numpy as np
+a = [1,2,3,4,5,6,7]
+def my_func(a):
+    return np.mean(a)
+print(my_func(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
